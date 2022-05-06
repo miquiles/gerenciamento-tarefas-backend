89826cc6eac6f409983417fd7416702f2b89f865
pedromiquiles <miquiles.lima@hotmail.com>
2022-05-05 23:27:39 -0300

    first commit

---
+++ src/main/java/gerenciamentotarefasbackend/GerenciamentoTarefasBackendApplication.java
@@ -0,0 +1,13 @@
+package gerenciamentotarefasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoTarefasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoTarefasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gerenciamentotarefasbackend/GerenciamentoTarefasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gerenciamentotarefasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoTarefasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
